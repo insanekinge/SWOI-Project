fe52be82f0dbcf2143749c089950cadf48833984
Кинге Владислав Дмитриевич <VKinge@synergy.ru>
2023-08-01 18:17:35 +0300

    main add new project

--- js/main.js
+++ js/main.js
@@ -1,44 +1,8 @@
+const container = document.querySelector(".container");
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-$(document).ready(function(){
-
-  $('.slide-images').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    dots: true,
-    variableWidth: true,
-    arrows: false
-
-  });
+container.addEventListener("wheel", (event) => {
+  event.preventDefault();
+  
+  // Determines if the user scrolls left or right and adjusts the scroll value accordingly
+  container.scrollLeft += event.deltaY + event.deltaX;
 });
-
-let popup = $('.popup');
-let link = document.querySelector('.footer__feedback');
-
-
-link.addEventListener('click', function() {
-  
-});
-
-let navList = document.querySelector('.nav__list')
-let burgerBtn = document.querySelector('.burger-menu')
-
-burgerBtn.onclick = function(e) {
-  navList.classList.toggle('active');
-  
-  burgerBtn.classList.toggle('active-burger');
-  
-  burgerBtn.classList.add('animate__flipInX');
-  navList.classList.add('animate__fadeIn');
-  setTimeout(() => {
-    burgerBtn.classList.remove('animate__flipInX');
-    navList.classList.remove('animate__fadeIn');
-    
-  }, 300);
-  
-  
-}
